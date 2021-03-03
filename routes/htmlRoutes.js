ccb258501cd25994d91eae5b77916aa0ce988acf
Carter Venne <you@example.com>
2021-03-03 16:47:59 -0600

    htmlroute comment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+//routing to notes page and home page
 module.exports = function (app) {
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
