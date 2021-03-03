38d78a06cc3f1694077510c8833019439f3c4ea0
Carter Venne <you@example.com>
2021-03-03 16:46:48 -0600

    sever connection w/comments

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3306;
+//port
+const PORT = process.env.PORT || 8080;
 
+//data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//setup public folder to be accesible
+app.use(express.static("public"));
+
+//routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//listener
 app.listen(PORT, function() {
     console.log('app listening on PORT: ' + PORT);
 });
